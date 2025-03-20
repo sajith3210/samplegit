33d1ddc6839d96063e1bdaf2d92144a55e67a05e
sajith3210 <sajithjanardhanan123@gmail.com>
2025-03-20 16:28:43 +0530

    login page 75%

--- gitapp/views.py
+++ gitapp/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 def index(request):
     if request.method=="GET":
         return render(request,'index.html')
+    
+
+def login(request):
+    if request.method=="GET":
+        return render(request,'login.html')
