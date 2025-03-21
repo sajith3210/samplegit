860779fb5b34b3aeaa63873e6f1e288ae52525bd
sajith3210 <sajithjanardhanan123@gmail.com>
2025-03-21 19:02:37 +0530

    Reg page

--- gitapp/urls.py
+++ gitapp/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path("", views.index,name=""),
     path("login", views.login,name="login"),
+    path("register", views.register,name="register"),
 ]
--- gitapp/views.py
+++ gitapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .forms import RegisterForm
 # Create your views here.
 
 def index(request):
@@ -10,3 +11,15 @@ def index(request):
 def login(request):
     if request.method=="GET":
         return render(request,'login.html')
+    
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()  # Save user to database
+            login(request, user)  # Log the user in
+            return redirect("home")  # Redirect to home page
+    else:
+        form = RegisterForm()
+
+    return render(request, "register.html", {"form": form})
